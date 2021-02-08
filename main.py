b836d55c8c557a38b5562ad187d8ebb99bd314bb
nikita <nikita03565@gmail.com>
2021-02-08 14:22:50 +0300

    done first part

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from utils import parse_table, to_json
+
 path = 'C:\Program Files (x86)\Selenium\chromedriver.exe'
 driver = webdriver.Chrome(path)
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+url = 'https://apps.irs.gov/app/picklist/list/priorFormPublication.html;jsessionid=fc15GG-KNts4sTUfTXSuaSBE.20?resultsPerPage=200&sortColumn=sortOrder&indexOfFirstRow=0&criteria=&value=&isDescending=false'
+
+search_terms = "Form W-2", "Form 1095-C"
+
+term = search_terms[0]
+
+driver.get(url)
+
+for term in search_terms:
+    search = driver.find_element_by_id("searchFor")
+    search.clear()
+    search.send_keys(term)
+    search.send_keys(Keys.RETURN)
+    res = parse_table(driver.page_source)
+    print(to_json(res, term))
+
 driver.close()
---
+++ utils.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+
+
+def parse_table(html):
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find('table', {'class': 'picklist-dataTable'})
+    header, *rows = table.find_all('tr')
+    res = []
+    for row in rows:
+        ds = row.find_all('td')
+        res.append({
+            'form_number': ds[0].text.strip(), 'title': ds[1].text.strip(), 'year': int(ds[2].text),
+            'link': ds[0].a['href']
+        })
+    return res
+
+
+def to_json(data, term):
+    filtered = [d for d in data if d['form_number'] == term]
+    min_year = min(f['year'] for f in filtered)
+    max_record = max(filtered, key=lambda f: f['year'])
+    return {
+        'form_number': filtered[0]['form_number'],
+        'title': max_record['title'],
+        'min_year': min_year,
+        'max_year': max_record['year']
+    }
