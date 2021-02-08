c0b9bdb9b601e4f24cbffbe574342c48a9615d83
nikita <nikita03565@gmail.com>
2021-02-08 12:57:41 +0300

    inital

---
+++ main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+path = 'C:\Program Files (x86)\Selenium\chromedriver.exe'
+driver = webdriver.Chrome(path)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
