46bb6c6aa611d1ea48cecf8fe07887989c045df9
nikita <nikita03565@gmail.com>
2021-02-08 14:44:10 +0300

    started pdf download

--- download.py
+++ download.py
@@ -7,18 +7,20 @@ path = 'C:\Program Files (x86)\Selenium\chromedriver.exe'
 driver = webdriver.Chrome(path)
 url = 'https://apps.irs.gov/app/picklist/list/priorFormPublication.html;jsessionid=fc15GG-KNts4sTUfTXSuaSBE.20?resultsPerPage=200&sortColumn=sortOrder&indexOfFirstRow=0&criteria=&value=&isDescending=false'
 
-search_terms = "Form W-2", "Form 1095-C"
-
-term = search_terms[0]
+term = "Form W-2"
+start_year = 2000
+end_year = 2021
 
 driver.get(url)
+search = driver.find_element_by_id("searchFor")
+search.clear()
+search.send_keys(term)
+search.send_keys(Keys.RETURN)
+res = parse_table(driver.page_source)
 
-for term in search_terms:  # TODO листать страницы
-    search = driver.find_element_by_id("searchFor")
-    search.clear()
-    search.send_keys(term)
-    search.send_keys(Keys.RETURN)
-    res = parse_table(driver.page_source)
-    print(to_json(res, term))
+links = []
+for rec in res:
+    if rec['form_number'] == term and start_year <= rec['year'] <= end_year + 1:
+        links.append(rec['link'])
 
-driver.close()
+print(links)
